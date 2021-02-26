cd3f12ea93a21667de094a5c0e5fee6734bf664e
Simon Martineau <simon.martineau.3@ulaval.ca>
2021-02-25 23:00:56 -0500

    Added env deploy script

--- deployvars.ps1
+++ deployvars.ps1
@@ -1,2 +1,2 @@
-$envvars = (get-content ${PSScriptRoot}/instance/production.env) -join ' ' -replace '  ', ' '
+$envvars = (get-content "${PSScriptRoot}/instance/production.env") -join ' ' -replace '  ', ' '
 ssh -t dokku@asdf.simonmartineau.dev config:set projects-api $envvars
