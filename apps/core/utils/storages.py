8a4899e0b587336f60290b061bca18bb76e33630
Simon Martineau <simon.martineau.3@ulaval.ca>
2021-02-25 23:36:49 -0500

    Modified storage root

--- apps/core/utils/storages.py
+++ apps/core/utils/storages.py
@@ -2,10 +2,10 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
-    location = "static"
+    location = "projects-api/static"
     default_acl = "public-read"
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
-    location = "media"
+    location = "projects-api/media"
     file_overwrite = False
